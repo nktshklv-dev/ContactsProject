0c2699b6377a503781245cbf550dd09bd042ae79
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-05-11 17:53:02 +0300

    Initial Commit

---
+++ contacts (practice #2)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contacts (practice #2)
+//
+//  Created by Nikita  on 5/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
